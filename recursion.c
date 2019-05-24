0c7e78bf4aa9ee4c03a11fa9e5a85904b5491249
Matthias Gessinger <s6magess@uni-bonn.de>
2019-05-24 19:01:02 +0200

    No longer assumes radius of convergence of 1

--- recursion.c
+++ recursion.c
@@ -1,97 +1,103 @@
 #include "recursion.h"
 
-ulong find_power_series_regular(acb_t out, acb_ode_t ODE, acb_t in, slong bits) {
+ulong find_power_series_regular(acb_ode_t ODE, acb_t in, slong bits) {
     /* Iteratively compute the summands of the power series solution near in (where z0 = 0)
-     * Only converges when z0=0 is an ordinary point and and B(0,|in|) contains no singularities. */
+     * Only converges when z0=0 is an ordinary point and and B(0,|in|) contains no singularities.
+     * This is not tested here! */
     acb_t temp; acb_init(temp);
-    acb_t new_coeff; acb_init(new_coeff); /* Stores the latest coefficient which is being computed */
-    acb_t result;    acb_init(result);    /* Stores the result to allow for aliasing of out and in */
-    arf_t ubound;    arf_init(ubound);    /* Stores an upper bound for each old coefficient */
+    acb_t newCoeff; acb_init(newCoeff); /* Stores the latest coefficient which is being computed */
+    acb_t oldCoeff; acb_init(oldCoeff); /* Stores the old coefficient which is looped over */
+    acb_t power;    acb_init(power);    /* Stores the current power of in */
+    mag_t ubound;   mag_init(ubound);   /* Stores an upper bound for each old coefficient */
 
     if (in == NULL) /* No input means no output. */
     {
         flint_printf("No input was provided so no output will be computed.\n");
-        out = NULL;
     }
-    if (out != NULL)
-    {
-        acb_poly_truncate(ODE->series,order(ODE));
-        acb_poly_evaluate(result,ODE->series,in,bits);
-    }
+    else
+        acb_one(power);
+
     slong num_of_nonzero;
     slong newIndex = 0;
+    slong currentError, maxRealError, maxImagError;
     do {
         num_of_nonzero = 0;
-        acb_zero(new_coeff);
+        maxRealError = maxImagError = -bits;
+        acb_zero(newCoeff);
         slong minIndex = (newIndex > degree(ODE)) ? newIndex - degree(ODE) : 0;
+
         for (slong oldIndex = newIndex+order(ODE)-1; oldIndex >= minIndex; oldIndex--)
         {
+            acb_poly_get_coeff_acb(oldCoeff,ODE->series,oldIndex);
+
             /* Ignore zero terms immediately: */
-            if (acb_poly_get_coeff_ptr(ODE->series,oldIndex) == NULL)
+            if (acb_is_zero(oldCoeff))
                 continue;
-            if (acb_is_zero(acb_poly_get_coeff_ptr(ODE->series,oldIndex)))
-                continue;
-            acb_get_abs_ubound_arf(ubound,acb_poly_get_coeff_ptr(ODE->series,oldIndex),bits);
-            if (arf_cmp_2exp_si(ubound,-bits) >= 0)
+
+            /* Check for proper convergence of the summands */
+            acb_mul(temp,oldCoeff,power,bits);
+            acb_get_mag(ubound,temp);
+            if (mag_cmp_2exp_si(ubound,-bits) >= 0)
                 num_of_nonzero++;
             acb_zero(temp);
+
+            /* Check error bounds */
+            currentError = arb_rel_error_bits(acb_realref(oldCoeff));
+            if (currentError >= maxRealError)
+                maxRealError = currentError;
+            currentError = arb_rel_error_bits(acb_imagref(oldCoeff));
+            if (currentError >= maxImagError)
+                maxImagError = currentError;
+
             /* Loop through the polynomials */
             for (slong polyIndex = order(ODE); polyIndex >= 0; polyIndex--)
             {
                 /* No more than degree(ODE) terms can contribute: */
                 if (polyIndex > degree(ODE) + oldIndex - newIndex)
-                {
-                    polyIndex = degree(ODE) + oldIndex - newIndex + 1;
-                    continue;
-                }
+                    polyIndex = degree(ODE) + oldIndex - newIndex;
                 if (polyIndex > oldIndex)
                     polyIndex = oldIndex;
                 if (polyIndex + newIndex - oldIndex < 0)
                     break;
+
                 acb_add(temp,temp,diff_eq_coeff(ODE,polyIndex,polyIndex+newIndex-oldIndex),bits);
                 if (polyIndex != 0)
                     acb_mul_si(temp,temp,oldIndex - polyIndex + 1,bits);
             }
-            acb_addmul(new_coeff,acb_poly_get_coeff_ptr(ODE->series,oldIndex),temp,bits);
+            acb_addmul(newCoeff,oldCoeff,temp,bits);
         }
-        /* Divide by the appropriate coefficients */
+        /* Divide by the coefficient of a_b where b = newIndex + order(ODE) */
         acb_set_ui(temp,newIndex+1);
         acb_rising_ui(temp,temp,order(ODE),bits);
         acb_mul(temp,temp,diff_eq_coeff(ODE,order(ODE),0),bits);
         acb_neg(temp,temp);
+        acb_div(newCoeff,newCoeff,temp,bits);
 
-        acb_div(new_coeff,new_coeff,temp,bits);
-        acb_poly_set_coeff_acb(ODE->series,newIndex+order(ODE),new_coeff);
-
-        /* Compute the whole summand as a_b*z^b where b = newIndex + order(ODE) */
-        if (in != NULL)
+        if (!acb_is_finite(newCoeff))
         {
-            acb_pow_ui(temp,in,newIndex+order(ODE),bits);
-            acb_mul(temp,new_coeff,temp,bits);
+            flint_printf("A coefficient was evaluated to be NaN. Aborting.\n");
+            newIndex = NON_CONVERGENT;
+            break;
         }
-        else
-            acb_set(temp,new_coeff);
-        /* If necessary, do the summation */
-        if (out != NULL)
-            acb_add(result,result,temp,bits);
+        acb_poly_set_coeff_acb(ODE->series,newIndex+order(ODE),newCoeff);
+        acb_mul(power,power,in,bits);
 
-        if (++newIndex >= 5000)
+        if (++newIndex >= 2*bits)
         {
-            flint_printf("5000 summands have been computed and no convergence was achieved. Aborting.\n");
-            FILE *out = fopen("data/polydump.txt","a");
-            acb_poly_fprintd(out,ODE->series,10);
-            fclose(out);
-            newIndex = 0;
+            flint_printf("%w summands have been computed and no convergence was achieved. Aborting.\n",newIndex);
+            newIndex = NON_CONVERGENT;
             break;
         }
     } while (num_of_nonzero != 0);
+    if (newIndex == NON_CONVERGENT)
+        acb_ode_dump(ODE);
 
-    if (out != NULL)
-        acb_set(out,result);
+    flint_printf("The maximum relative Error amongst all coefficients was (%w,%w).\n",maxRealError,maxImagError);
 
-    arf_clear(ubound);
-    acb_clear(new_coeff);
-    acb_clear(result); acb_clear(temp);
+    mag_clear(ubound);
+    acb_clear(newCoeff); acb_clear(oldCoeff);
+    acb_clear(power); acb_clear(temp);
+
     return newIndex;
 }
 
@@ -107,7 +113,7 @@ void analytic_continuation (acb_t res, acb_ode_t ODE_in, acb_srcptr path, slong 
     {
         acb_ode_shift(ODE,a,bits);
         acb_sub(a,path+i,path+(i-1),bits);
-        if (find_power_series_regular(res,ODE,a,bits) == 0)
+        if (find_power_series_regular(ODE,a,bits) == 0)
             break;
         acb_poly_taylor_shift(ODE->series,ODE->series,a,bits);
         accuracy = acb_rel_accuracy_bits(res);
@@ -162,6 +168,7 @@ int checkODE (acb_poly_t *polys, acb_ode_t ODE, slong digits) {
         {
             if (printed == 0)
             {
+                flint_printf("The differential equation was not solved correctly. These are the coefficients:\n");
                 printed = 1;
                 acb_poly_printd(ODE->series,10);
                 flint_printf("\n\n");
@@ -189,9 +196,31 @@ void find_monodromy_matrix (acb_mat_t monodromy, acb_ode_t ODE, acb_ptr path, sl
     return;
 }
 
+void acb_ode_dump(acb_ode_t ODE)
+{
+    FILE *out = fopen("data/odedump.txt","w");
+    if (out == NULL)
+        return;
+    flint_fprintf(out,"Order: %w\nDegree: %w\n",order(ODE),degree(ODE));
+    for (slong i = 0; i <= order(ODE); i++)
+    {
+        for (slong j = 0; j < degree(ODE); j++)
+        {
+            acb_fprintd(out,diff_eq_coeff(ODE,i,j),10);
+            flint_fprintf(out,"\t");
+        }
+        flint_fprintf(out,"\n");
+    }
+    flint_fprintf(out,"\n");
+    acb_poly_fprintd(out,ODE->series,10);
+    flint_fprintf(out,"\n\n\n");
+    fclose(out);
+    return;
+}
+
 void entry_point (ulong maxOrder, slong digits, slong z_val, const char *file) {
     digits = digits*3.32193; /* Multiply by log_2(10) to obtain the number of binary digits */
-    slong steps = 128;
+    slong steps = 256;
 
     /* Polynomials */
     ulong numOfPols;
@@ -213,6 +242,7 @@ void entry_point (ulong maxOrder, slong digits, slong z_val, const char *file) {
         acb_exp_pi_i(path+i,path+i,steps/2*digits);
     }
     acb_set(path+steps,path);
+    _acb_vec_scalar_div_ui(path,path,steps+1,4,steps/2*digits);
 
     acb_ode_t ODE = acb_ode_init(polys,NULL,z,numOfPols,digits);
     if (ODE != NULL)
--- recursion.h
+++ recursion.h
@@ -9,12 +9,14 @@
 
 void analytic_continuation (acb_t res, acb_ode_t ODE_in, acb_srcptr path, slong len, slong prec, int output_series);
 
-ulong find_power_series_regular(acb_t res, acb_ode_t ODE, acb_t a, slong bits);
+ulong find_power_series_regular(acb_ode_t ODE, acb_t in, slong bits);
 
 void find_monodromy_matrix (acb_mat_t monodromy, acb_ode_t ODE, acb_ptr path, slong len, slong digits);
 
 int checkODE (acb_poly_t *polys, acb_ode_t ODE, slong digits);
 
 void entry_point (ulong n, slong prec, slong z, const char *file);
+
+void acb_ode_dump(acb_ode_t ODE);
 
 #endif
