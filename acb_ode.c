26225ec932c09df69ace641926f0fd9f26f5a7da
Matthias Gessinger <s6magess@uni-bonn.de>
2019-06-25 22:16:39 +0200

    Reordered function to conform with ANSI C

--- acb_ode.c
+++ acb_ode.c
@@ -1,20 +1,19 @@
 #include "acb_ode.h"
 
 short precondition (acb_poly_t *polys, acb_ode_t ODE) {
-    /* Error Checking */
-    if (polys == NULL) {
+    /* Exception handling */
+    if (polys == NULL)
+    {
         flint_printf("Please provide defining polynomials.\n");
         return INVALID_DATA;
     }
     /* Find the highest degree amongst the given polynomials */
-    slong polyMaxLength = 0, ord = ODE->order;
-    for (slong i = ord; i >= 0; i--)
+    slong polyMaxLength = 0, ord = order(ODE);
+    while (polys[ord] == NULL || acb_poly_is_zero(polys[ord]))
+        ord--;
+    for (slong i = 0; i <= ord; i++)
     {
-        if (polys[i] == NULL)
-        {
-            if (polyMaxLength == 0) ord--;
-        }
-        else if (polyMaxLength < acb_poly_length(polys[i]))
+        if (polyMaxLength < acb_poly_length(polys[i]))
             polyMaxLength = acb_poly_length(polys[i]);
     }
     order(ODE) = ord;
@@ -36,22 +35,21 @@ acb_ode_t acb_ode_init (acb_poly_t *polys, acb_poly_t initial, slong order) {
         return NULL;
     }
     order(ODE) = order;
-    int ode_case = precondition(polys,ODE);
-    if (ode_case != ORDINARY) {
+    if (precondition(polys,ODE) != ORDINARY) {
         flint_free(ODE);
         return NULL;
     }
     ODE->polys = flint_malloc((order(ODE)+1)*sizeof(acb_ptr));
     for (slong i = 0; i <= order(ODE); i++) {
-        (ODE->polys)[i] = _acb_vec_init(degree(ODE)+1);
+        diff_eq_poly(ODE,i) = _acb_vec_init(degree(ODE)+1);
         if (polys[i] == NULL) continue;
         for (slong j = 0; j <= acb_poly_degree(polys[i]); j++) {
             acb_poly_get_coeff_acb(diff_eq_coeff(ODE,i,j),polys[i],j);
         }
     }
-    acb_poly_init(ODE->series);
+    acb_poly_init(ODE->solution);
     if (initial != NULL)
-        acb_poly_set(ODE->series,initial);
+        acb_poly_set(ODE->solution,initial);
     return ODE;
 }
 
@@ -61,7 +59,7 @@ void acb_ode_clear (acb_ode_t ODE) {
         _acb_vec_clear((ODE->polys)[i],degree(ODE)+1);
     }
     flint_free(ODE->polys);
-    acb_poly_clear(ODE->series);
+    acb_poly_clear(ODE->solution);
     flint_free(ODE);
     return;
 }
@@ -83,25 +81,28 @@ acb_ode_t acb_ode_set (acb_ode_t ODE_out, acb_ode_t ODE_in) {
         {
             (ODE_out->polys)[i] = _acb_vec_init(degree(ODE_out)+1);
         }
-        acb_poly_init(ODE_out->series);
+        acb_poly_init(ODE_out->solution);
     }
     for (slong i = 0; i <= order(ODE_out); i++)
     {
         _acb_vec_set(diff_eq_poly(ODE_out,i),diff_eq_poly(ODE_in,i),degree(ODE_out)+1);
     }
-    acb_poly_set(ODE_out->series,ODE_in->series);
+    acb_poly_set(ODE_out->solution,ODE_in->solution);
     return ODE_out;
 }
 
 void acb_ode_shift (acb_ode_t ODE, acb_t a, slong bits)
 {
-    if (degree(ODE) >= 1)
-        for (slong j = 0; j <= order(ODE); j++)
-        {
-            if (ODE->polys[j] == NULL)
-                continue;
-            _acb_poly_taylor_shift(ODE->polys[j],a,degree(ODE)+1,bits);
-        }
+    if (degree(ODE) < 1)
+        return;
+    for (slong j = 0; j <= order(ODE); j++)
+    {
+        if (diff_eq_poly(ODE,j) == NULL)
+            continue;
+        if (_acb_vec_is_zero(diff_eq_poly(ODE,j),degree(ODE)+1))
+            continue;
+        _acb_poly_taylor_shift(diff_eq_poly(ODE,j),a,degree(ODE)+1,bits);
+    }
     return;
 }
 
