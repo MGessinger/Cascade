4d1d76b2fdfa815f5e4473d1c990b044eccc4694
Matthias Gessinger <s6magess@uni-bonn.de>
2020-03-21 12:46:21 +0100

    Isolate predefined ODEs in Jade
    Move predefined constructors to examples.jl and make their names consistent

--- julia/Jade.jl
+++ julia/Jade.jl
@@ -2,7 +2,7 @@ module Jade
 
 using Nemo
 
-export diffOp,jade_ode,acb_ode,arb_ode,monodromy,powerSeries,setPolynomial!,jade_ode_legendre,acb_ode_bessel,acb_ode_hypgeom
+export diffOp,jade_ode,acb_ode,arb_ode,monodromy,powerSeries,setPolynomial!,jade_ode_legendre,jade_ode_bessel,jade_ode_hypgeom
 
 abstract type diffOp end
 
@@ -164,30 +164,8 @@ function monodromy(ode::jade_ode,z0=0)
 	return mono
 end
 
-function jade_ode_legendre(R::PolyRing,n::Integer)
-	ode = jade_ode([R(n*(n+1)),R([0,-2]),R([1,0,-1])])
-	ode.odeC = ccall((:acb_ode_legendre,:libcascade),Ptr{Nothing},(Cint,),n)
-	return ode
-end
-
-function acb_ode_bessel(R::AcbPolyRing,nu::acb)
-	p = R([0,0,1])
-	ode = acb_ode([p-nu*nu,R([0,1]),R([0,0,1])])
-	N = Ref{acb}(nu)
-	ode.odeC = ccall((:acb_ode_bessel,:libcascade),Ptr{Nothing},(Ref{acb},Cint),N,R.base_ring.prec)
-	return ode
-end
-
-function acb_ode_hypgeom(R::AcbPolyRing,a::acb,b::acb,c::acb)
-	ode = acb_ode([R(-a*b),R([c,(a+b+1)]),R([0,1,-1])])
-	A = Ref{acb}(a)
-	B = Ref{acb}(b)
-	C = Ref{acb}(c)
-	ode.odeC = ccall((:acb_ode_hypgeom,:libcascade),Ptr{Nothing},(Ref{acb},Ref{acb},Ref{acb},Cint),A,B,C,R.base_ring.prec)
-	return ode
-end
-
 # External code files come here
+include("examples.jl")
 include("arith.jl")
 
 end
